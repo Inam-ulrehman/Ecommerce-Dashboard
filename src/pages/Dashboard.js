213aea3e20bf888d1ed605bc7fc0657d942715ed
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-11-30 12:38:43 -0500

    fix

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet-async'
 import { useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 import styled from 'styled-components'
-import { DashboardSideBar } from '../components/dashboard'
+import DashboardSideBar from '../components/dashboard/DashboardSideBar'
 
 const Dashboard = () => {
   const { user } = useSelector((state) => state)
