8b91ca41a2abe5a258e09eee8c49d3d4be8f4e63
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-12-01 11:21:58 -0500

    fix

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useEffect } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import { getProductsThunk } from '../features/products/productSlice'
 
 const Products = () => {
@@ -22,17 +24,54 @@ const Products = () => {
     )
   }
   return (
-    <div>
+    <Wrapper>
       <Helmet>
         <title>Product</title>
         <meta name='description' content='Welcome to our Product Page.' />
         <link rel='canonical' href='/product' />
       </Helmet>
-      {productsList.map((item) => {
-        return <div key={item._id}>hello</div>
-      })}
-    </div>
+      <table>
+        <tbody>
+          <tr>
+            <th>PRODUCT IMAGE</th>
+            <th>TITLE</th>
+            <th>CATEGORY</th>
+            <th>AVAILABLE</th>
+            <th>ACTIONS</th>
+          </tr>
+
+          {productsList.map((item) => {
+            return (
+              <tr key={item._id}>
+                <td className='image-holder'>
+                  <img src={item.uploadImage[0].secure_url} alt={item.title} />
+                </td>
+                <td>{item.title}</td>
+                <td>{item.category}</td>
+                <td>{item.inStock ? 'In-Stock' : 'out-of-Stock'}</td>
+                <td>
+                  <Link className='btn' to={item._id}>
+                    Edit
+                  </Link>
+                  <button type='button' className='btn'>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </Wrapper>
   )
 }
-
+const Wrapper = styled.div`
+  text-align: center;
+  .image-holder {
+    max-width: 150px;
+  }
+  img {
+    width: 100px;
+  }
+`
 export default Products
