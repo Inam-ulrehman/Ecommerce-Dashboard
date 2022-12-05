7efb5af1e85b5a339131d4a851355e0224d47fdb
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-12-05 10:45:38 -0500

    fix

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -1,5 +1,5 @@
 const paginate = (data) => {
-  const itemsPerPage = 2
+  const itemsPerPage = 20
   const pages = Math.ceil(data.length / itemsPerPage)
   const newData = Array.from({ length: pages }, (_, index) => {
     const start = index * itemsPerPage
