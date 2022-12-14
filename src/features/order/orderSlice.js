6f201c60861d47ddaea27274fd54f35995dcd42a
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-12-14 08:53:27 -0500

    fix

---
+++ src/features/order/orderSlice.js
@@ -0,0 +1,48 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { customFetch } from '../../utils/axios'
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isLoading: false,
+}
+
+export const orderThunk = createAsyncThunk(
+  'order/orderThunk',
+  async (_, thunkAPI) => {
+    try {
+      const response = await customFetch.get()
+      console.log('hello Thunk')
+      return response.data
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data)
+    }
+  }
+)
+
+const orderSlice = createSlice({
+  name: 'order',
+  initialState,
+  reducers: {
+    createFunction: (state, { payload }) => {
+      console.log('function call')
+    },
+  },
+  extraReducers: {
+    [orderThunk.pending]: (state, { payload }) => {
+      console.log('promise pending')
+      state.isLoading = true
+    },
+    [orderThunk.fulfilled]: (state, { payload }) => {
+      console.log('promise full filled')
+      state.isLoading = false
+    },
+    [orderThunk.rejected]: (state, { payload }) => {
+      console.log('promise rejected')
+      state.isLoading = false
+    },
+  },
+})
+export const { createFunction } = orderSlice.actions
+export default orderSlice.reducer
--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import contactSlice from './features/contact/contactSlice'
 import functionSlice from './features/functions/functionSlice'
+import orderSlice from './features/order/orderSlice'
 import editProductSlice from './features/products/editProductSlice'
 import productSlice from './features/products/productSlice'
 import userSlice from './features/user/userSlice'
@@ -12,6 +13,7 @@ const store = configureStore({
     editProduct: editProductSlice,
     function: functionSlice,
     contact: contactSlice,
+    order: orderSlice,
   },
 })
 
