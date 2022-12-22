619f0083c886013962e7c2397ae48faac671b147
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-12-22 09:11:13 -0500

    fix

--- src/pages/dashboardPages/DashboardHome.js
+++ src/pages/dashboardPages/DashboardHome.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+import { useSelector } from 'react-redux'
+import { DashboardTotalCard } from '../../components/cards'
 
 const DashboardHome = () => {
-  return <div>DashboardHome</div>
-}
+  const { product } = useSelector((state) => state)
 
+  return (
+    <Wrapper>
+      <DashboardTotalCard
+        total={product.nbHits}
+        navigateLink={'/dashboard/products'}
+      />
+    </Wrapper>
+  )
+}
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1rem;
+  gap: 1rem;
+  justify-content: center;
+`
 export default DashboardHome
