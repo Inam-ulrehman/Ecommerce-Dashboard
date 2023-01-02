a1e7b336ea8e4ddebc6d9bcf897aa00465878b9a
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-01-02 11:19:26 -0500

    fix

--- src/components/dashboard/WebsiteContentComponents/ContentSectionOne.js
+++ src/components/dashboard/WebsiteContentComponents/ContentSectionOne.js
@@ -1,7 +1,82 @@
 import React from 'react'
+import { useState } from 'react'
+import styled from 'styled-components'
+import FormInput from '../../FormInput'
 
+const initialState = {
+  heading: '',
+  buttonTitle: '',
+  desktopImage: '',
+  mobileImage: '',
+  paragraph: '',
+}
 const ContentSectionOne = () => {
-  return <div>ContentSectionOne</div>
+  const [state, setState] = useState(initialState)
+
+  const handleSubmit = (e) => {
+    console.log(e)
+  }
+  const handleChange = (e) => {
+    const value = e.target.value
+    const name = e.target.name
+    setState({ ...state, [name]: value })
+  }
+  return (
+    <Wrapper>
+      <h3>Section-1</h3>
+      <form className='form' onSubmit={handleSubmit}>
+        {/* heading  */}
+        <div>
+          <FormInput
+            label={'Heading'}
+            name={'heading'}
+            value={state.heading}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Button Title  */}
+        <div>
+          <FormInput
+            label={'Button Title'}
+            name={'buttonTitle'}
+            value={state.buttonTitle}
+            onChange={handleChange}
+          />
+        </div>
+        {/* desktop Image */}
+        <div>
+          <FormInput
+            label={'Desktop Image'}
+            name={'desktopImage'}
+            value={state.desktopImage}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Mobile Image */}
+        <div>
+          <FormInput
+            label={'Mobile Image'}
+            name={'mobileImage'}
+            value={state.mobileImage}
+            onChange={handleChange}
+          />
+        </div>
+        {/* paragraph */}
+        <div>
+          <FormInput
+            label={'Paragraph'}
+            name={'paragraph'}
+            value={state.paragraph}
+            onChange={handleChange}
+          />
+        </div>
+        <button type='submit' className='btn'>
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  )
 }
 
+const Wrapper = styled.div``
 export default ContentSectionOne
