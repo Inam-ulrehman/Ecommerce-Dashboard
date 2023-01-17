330700711d83b88320227f6d2b5958408a4bd7c4
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-01-17 17:59:58 -0500

    fix

--- src/components/user/UserList.js
+++ src/components/user/UserList.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import { getUsersThunk } from '../../features/user/userSlice'
+
 import { formatDate } from '../../utils/helper'
 
 const UserList = () => {
