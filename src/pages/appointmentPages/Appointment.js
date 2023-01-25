4a84c63acc4c9aa1308622f4ac108dac540870d1
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-01-25 08:46:35 -0500

    fix

--- src/pages/appointmentPages/Appointment.js
+++ src/pages/appointmentPages/Appointment.js
@@ -1,21 +1,41 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import { List, Pagination, Search } from '../../components/appointment'
 
 const Appointment = () => {
+  const { count, page } = useSelector((state) => state.appointment)
   return (
-    <div>
-      <div className='btn-holder'>
-        <Link to={`register`} className='btn'>
-          Register Appointment
+    <Wrapper>
+      <div className='container-heading'>
+        <Link className='btn' to={'register'}>
+          Register New User
         </Link>
+
+        <span>
+          Total Results:
+          <strong> {count}</strong>
+        </span>
+        <span>
+          Page No:<strong>{page}</strong>
+        </span>
       </div>
       <Search />
       <List />
       <Pagination />
-    </div>
+    </Wrapper>
   )
 }
+const Wrapper = styled.div`
+  .container-heading {
+    display: flex;
+    justify-content: space-between;
+    span {
+      padding: 0 2rem;
+    }
+  }
+`
 
 export default Appointment
