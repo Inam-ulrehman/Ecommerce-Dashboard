f7d321ac3564bec33b4a5412560cb418c66096de
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-01-27 15:49:09 -0500

    fix

--- src/components/contact/Search.js
+++ src/components/contact/Search.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import styled from 'styled-components'
+import { BiSearchAlt2 } from 'react-icons/bi'
 import {
   getContactThunk,
   getStateValues,
 } from '../../features/contact/contactSlice'
+import SearchWrapper from '../../Wrapper/dashboard/SearchWrapper'
 
 const Search = () => {
   const dispatch = useDispatch()
@@ -47,91 +48,73 @@ const Search = () => {
   }, [searchName, searchEmail, searchPhone, sort, limit, page, refreshData])
 
   return (
-    <Wrapper className='container'>
+    <SearchWrapper className='container'>
       <button className='btn clear-filter' type='button' onClick={handleClear}>
         Clear Filter
       </button>
-
-      <div className='limit-sort'>
-        <div className='limit'>
-          <label htmlFor='limit'>Limit</label>
-          <select name='limit' id='limit' onChange={handleChange}>
-            <option value={10}>10</option>
-            <option value={20}>20</option>
-            <option value={30}>30</option>
-            <option value={40}>40</option>
-          </select>
-        </div>
-        <div className='sort'>
-          <label htmlFor='sort'>Sort</label>
-          <select name='sort' id='sort' value={sort} onChange={handleChange}>
-            <option value='-createdAt'>SELECT OPTIONS</option>
-            <option value='-createdAt'>DATE NEW</option>
-            <option value='createdAt'>DATE OLD</option>
-            <option value='title'>NAME A-Z</option>
-            <option value='-title'>NAME Z-A</option>
-          </select>
+      <div className='limit-sort-input'>
+        <div className='limit-sort'>
+          <div className='limit'>
+            <label htmlFor='limit'>Limit</label>
+            <select name='limit' id='limit' onChange={handleChange}>
+              <option value={10}>10</option>
+              <option value={20}>20</option>
+              <option value={30}>30</option>
+              <option value={40}>40</option>
+            </select>
+          </div>
+          <div className='sort'>
+            <label htmlFor='sort'>Sort</label>
+            <select name='sort' id='sort' value={sort} onChange={handleChange}>
+              <option value='-createdAt'>SELECT OPTIONS</option>
+              <option value='-createdAt'>DATE NEW</option>
+              <option value='createdAt'>DATE OLD</option>
+              <option value='title'>NAME A-Z</option>
+              <option value='-title'>NAME Z-A</option>
+            </select>
+          </div>
         </div>
       </div>
       {/* ============box divided */}
       <div className='search'>
-        <input
-          type='text'
-          placeholder='First Name'
-          name='searchName'
-          value={searchName}
-          onChange={handleChange}
-        />
-        <input
-          type='email'
-          placeholder='Email'
-          name='searchEmail'
-          value={searchEmail}
-          onChange={handleChange}
-        />
-        <input
-          type='number'
-          placeholder='Phone Number'
-          name='searchPhone'
-          value={searchPhone}
-          onChange={handleChange}
-        />
+        {/* text */}
+        <div>
+          <input
+            type='text'
+            placeholder='First Name'
+            name='searchName'
+            value={searchName}
+            onChange={handleChange}
+          />
+          <BiSearchAlt2 />
+        </div>
+
+        {/* email */}
+        <div>
+          <input
+            type='email'
+            placeholder='Email'
+            name='searchEmail'
+            value={searchEmail}
+            onChange={handleChange}
+          />
+          <BiSearchAlt2 />
+        </div>
+
+        {/* phone */}
+        <div>
+          <input
+            type='number'
+            placeholder='Phone Number'
+            name='searchPhone'
+            value={searchPhone}
+            onChange={handleChange}
+          />
+          <BiSearchAlt2 />
+        </div>
       </div>
-    </Wrapper>
+    </SearchWrapper>
   )
 }
-const Wrapper = styled.div`
-  position: relative;
-  .clear-filter {
-    position: absolute;
-    top: 0;
-    right: 5%;
-  }
-  .limit-sort {
-    display: flex;
-    margin-bottom: 5px;
-    gap: 1rem;
-  }
-  .sort,
-  .limit {
-    select {
-      :hover {
-        cursor: pointer;
-      }
-    }
-  }
 
-  button {
-    border: transparent;
-
-    box-shadow: var(--shadow-1);
-    padding: 4px;
-    transition: var(--transition);
-    :hover {
-      cursor: pointer;
-      background-color: var(--primary-5);
-      color: var(--white);
-    }
-  }
-`
 export default Search
