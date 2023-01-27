37bf1cd9434668d1c32f43b5376e6c1658fda9de
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-01-27 13:05:29 -0500

    fix

--- src/pages/contactPages/Contact.js
+++ src/pages/contactPages/Contact.js
@@ -1,56 +1,18 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import styled from 'styled-components'
-import { List } from '../../components/contact'
+import { Heading, List } from '../../components/contact'
 import Pagination from '../../components/contact/Pagination'
 import Search from '../../components/contact/Search'
 
 const Contact = () => {
-  const { page, count } = useSelector((state) => state.contact)
-
   return (
     <Wrapper>
-      <div className='container-heading'>
-        <span></span>
-        <span>
-          Total Results:
-          <strong> {count}</strong>
-        </span>
-        <span>
-          Page No:<strong>{page}</strong>
-        </span>
-      </div>
+      <Heading />
       <Search />
       <List />
       <Pagination />
     </Wrapper>
   )
 }
-const Wrapper = styled.div`
-  .container-heading {
-    display: flex;
-    justify-content: space-between;
-    span {
-      padding: 0 2rem;
-    }
-  }
-  h4 {
-    display: flex;
-    justify-content: space-between;
-  }
-  .buttons {
-    width: 160px;
-    a {
-      margin: 5px;
-      padding: 1px 9px;
-    }
-  }
-
-  .tr {
-    text-align: center;
-    td {
-      text-transform: capitalize;
-    }
-  }
-`
+const Wrapper = styled.div``
 export default Contact
