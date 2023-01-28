42434dda1aada79c97a3f31fb3a3ca72c14ed39a
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-01-28 08:49:01 -0500

    fix

--- src/pages/appointmentPages/SingleAppointment.js
+++ src/pages/appointmentPages/SingleAppointment.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
 import { SelectedAppointment } from '../../components/appointment'
 import AppointmentDate from '../../components/appointment/AppointmentDate'
 import AvailableSlots from '../../components/appointment/AvailableSlots'
@@ -25,14 +26,20 @@ const SingleAppointment = () => {
     // eslint-disable-next-line
   }, [])
   return (
-    <div>
-      <Category />
-      <AppointmentDate />
-      <SelectedAppointment />
+    <Wrapper>
+      <div className='category-appointment'>
+        <Category />
+        <AppointmentDate />
+        <SelectedAppointment />
+      </div>
       <AvailableSlots />
       <CustomerDetails action={updateAppointmentThunk} />
-    </div>
+    </Wrapper>
   )
 }
-
+const Wrapper = styled.div`
+  .category-appointment {
+    display: flex;
+  }
+`
 export default SingleAppointment
